39b44ffc5477faf4d79f1504977dd104f80168b8
thanhto24 <thanhtovntmk@gmail.com>
2025-03-30 15:29:38 +0700

    Add error message when empty list on All model

---
+++ App/Converter/IntegerToVisibilityMsgConverter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace App.Converter
+{
+    public class IntegerToVisibilityMsgConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (int)value > 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
